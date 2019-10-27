820e0f2348194f018434fb09f4be546d8bec3f7d
Colin Mills <cgm71@georgetown.edu>
2019-10-27 19:31:52 -0400

    Update test file

--- TestSlipperySlot.java
+++ TestSlipperySlot.java
@@ -1,7 +1,7 @@
-import org.junit.jupiter.api.*;
- 
-//org.junit.jupiter.api.*;
+//import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.*;
+import static org.junit.Assert.*;
 
 
 public class TestSlipperySlot {
@@ -11,14 +11,30 @@ public class TestSlipperySlot {
 
         SlipperySlot test = new SlipperySlot();
 
-        //Create jackpot array that does not meet any other requiriments (ie. 42, perfect square)
+        //Create jackpot array that does not meet any other requirements (ie. 42, perfect square)
         int[] array = new int[]{10, 10, 10, 10, 10};
 
         int correctPayoff = 1000000;
 
-        assertEquals(correctPayoff, test.payOff(array), "Error with payoff in testJackpot");
+        assertEquals(correctPayoff, test.payOff(array));
 
-    }
+
+    }//END testJackpot
+
+    @Test
+    public void testNoPayout() {
+
+        SlipperySlot test = new SlipperySlot();
+
+        //Create jackpot array that does not meet any  requirements (ie. 42, perfect square)
+        int[] array = new int[]{3, 7, 11, 17, 19};
+
+        int correctPayoff = 0;
+
+        assertEquals(correctPayoff, test.payOff(array));
+
+
+    }//END testJackpot
 
 
 
