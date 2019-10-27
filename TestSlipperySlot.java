99db975005c4d3b6d7cc5d33c27a7c6114826c71
Colin Mills <cgm71@georgetown.edu>
2019-10-27 16:41:33 -0400

    Create TestSlipperySlot.java

---
+++ TestSlipperySlot.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api;
+
+public class TestSlipperySlots {
+
+    @Test
+    public void testJackpot() {
+
+        SlipperySlot test = new SlipperySlot();
+
+        //Create jackpot array that does not meet any other requiriments (ie. 42, perfect square)
+        int[] array = new int[]{10, 10, 10, 10, 10};
+
+        int correctPayoff = 1000000;
+
+        assertEquals(correctPayoff, test.payOff(array), "Error with payoff in testJackpot");
+
+    }
+
+
+
+} //END TestSlipperySlots
